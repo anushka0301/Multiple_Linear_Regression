fe4ebbf89e274d66fc2bac858b7e90b0812454b5
Anushka Chaudhuri <59223517+anushka0301@users.noreply.github.com>
2020-09-02 19:37:04 +0530

    Add files via upload

---
+++ MultipleLinearRegression.py
@@ -0,0 +1,55 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+
+#Importing the datasets
+dataset=pd.read_csv('50_Startups.csv')
+X=dataset.iloc[:, :-1].values
+Y=dataset.iloc[:, -1].values
+
+#Encoding categorical data
+from sklearn.compose import ColumnTransformer
+from sklearn.preprocessing import OneHotEncoder
+ct = ColumnTransformer(transformers=[('encoder', OneHotEncoder(), [3])], remainder='passthrough')
+X = np.array(ct.fit_transform(X))
+
+#Avoiding Dummy Variable Trap
+X=X[:, 1:]
+
+#Splitting the dataset into Training and Test sets
+from sklearn.model_selection import train_test_split
+X_train, X_test, Y_train, Y_test=train_test_split(X, Y, test_size=0.2, random_state=0)
+
+#Feature scaling
+"""
+from sklearn.preprocessing import StandardScaler
+sc_X=StandardScaler()
+X_train=sc_X.fit_transform(X_train)
+X_test=sc_X.transform(X_test)
+"""
+
+#Fitting Multiple Linear Regression to the Training set
+from sklearn.linear_model import LinearRegression
+regressor=LinearRegression()
+regressor.fit(X_train, Y_train)
+
+#Predicting the Test set results
+y_pred=regressor.predict(X_test)
+
+#===============================================================================
+#Building the optimal model using Backward elimination
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
